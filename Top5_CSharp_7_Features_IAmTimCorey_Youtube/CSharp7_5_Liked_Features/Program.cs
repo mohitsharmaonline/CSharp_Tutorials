209cb65564511b4dc92fee2ae306114032406b75
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-05 12:01:40 +0530

    Fourth feature: Throw in expression.

--- Top5_CSharp_7_Features_IAmTimCorey_Youtube/CSharp7_5_Liked_Features/Program.cs
+++ Top5_CSharp_7_Features_IAmTimCorey_Youtube/CSharp7_5_Liked_Features/Program.cs
@@ -45,60 +45,41 @@ namespace CSharp7_5_Liked_Features
             //}
 
 
-            // Feature 3: Powerful switch statements
+            //// Feature 3: Powerful switch statements
+            //Employee emp1 = new Employee { FirstName = "Joe", LastName = "Smith", IsManager = false, YearsWorked = 2 };
+            //Employee emp2 = new Employee { FirstName = "Sandra", LastName = "Jones", IsManager = true, YearsWorked = 28 };
+            //Customer cust1 = new Customer { FirstName = "Tim", LastName = "Corey", TotalDollarsSpent = 2342.15M };
+            //Customer cust2 = new Customer { FirstName = "Delana", LastName = "Greenback", TotalDollarsSpent = 558M };
+            //List<object> people = new List<object> { emp1, emp2, cust1, cust2 };
+
+            //foreach (var p in people)
+            //{
+            //    switch(p)
+            //    {
+            //        case Employee e when (e.IsManager == false):
+            //            Console.WriteLine($"{e.FirstName} is a good employee.");
+            //            break;
+            //        case Employee e when (e.IsManager):
+            //            Console.WriteLine($"{e.FirstName} runs the company.");
+            //            break;
+            //        case Customer c when (c.TotalDollarsSpent > 1000):
+            //            Console.WriteLine($"{c.FirstName} is a loyal customer.");
+            //            break;
+            //        case Customer c:
+            //            Console.WriteLine($"{c.FirstName} needs to spend more money.");
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
+
+            // Feature No. 4 : Throw in expression.
             Employee emp1 = new Employee { FirstName = "Joe", LastName = "Smith", IsManager = false, YearsWorked = 2 };
             Employee emp2 = new Employee { FirstName = "Sandra", LastName = "Jones", IsManager = true, YearsWorked = 28 };
-            Customer cust1 = new Customer { FirstName = "Tim", LastName = "Corey", TotalDollarsSpent = 2342.15M };
-            Customer cust2 = new Customer { FirstName = "Delana", LastName = "Greenback", TotalDollarsSpent = 558M };
-            List<object> people = new List<object> { emp1, emp2, cust1, cust2 };
+            List<Employee> people = new List<Employee> { emp1, emp2 };
 
-            foreach (var p in people)
-            {
-                switch(p)
-                {
-                    case Employee e when (e.IsManager == false):
-                        Console.WriteLine($"{e.FirstName} is a good employee.");
-                        break;
-                    case Employee e when (e.IsManager):
-                        Console.WriteLine($"{e.FirstName} runs the company.");
-                        break;
-                    case Customer c when (c.TotalDollarsSpent > 1000):
-                        Console.WriteLine($"{c.FirstName} is a loyal customer.");
-                        break;
-                    case Customer c:
-                        Console.WriteLine($"{c.FirstName} needs to spend more money.");
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Employee ceo = people.Where(emp => emp.IsManager).FirstOrDefault() ?? throw new Exception("Tehre was a problem finding a manager.");
+            Console.WriteLine($"The ceo is {ceo.FirstName}");
         }
     }
 }

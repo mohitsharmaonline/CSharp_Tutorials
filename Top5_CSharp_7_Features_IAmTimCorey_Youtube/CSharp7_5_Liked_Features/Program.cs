7d4e47b3045d48bb38b3871b8a72e278220d4514
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-05 11:21:56 +0530

    Second feature: Pattern Matching, complete example part 1

--- Top5_CSharp_7_Features_IAmTimCorey_Youtube/CSharp7_5_Liked_Features/Program.cs
+++ Top5_CSharp_7_Features_IAmTimCorey_Youtube/CSharp7_5_Liked_Features/Program.cs
@@ -10,19 +10,41 @@ namespace CSharp7_5_Liked_Features
     {
         static void Main(string[] args)
         {
-            // first feature: Inline out variables.
+            //// first feature: Inline out variables.
 
-            Console.Write("What is your age: ");
-            string ageText = Console.ReadLine();
+            //Console.Write("What is your age: ");
+            //string ageText = Console.ReadLine();
 
-            //// Old way of doing things
-            //int age = 0;
-            //bool IsValidAge = int.TryParse(ageText, out age);
+            ////// Old way of doing things
+            ////int age = 0;
+            ////bool IsValidAge = int.TryParse(ageText, out age);
 
-            // new ways of doing things.
-            bool IsValidAge = int.TryParse(ageText, out int age);
+            //// new ways of doing things.
+            //bool IsValidAge = int.TryParse(ageText, out int age);
 
-            Console.WriteLine($"Your age is { age }.");
+            //Console.WriteLine($"Your age is { age }.");
+
+
+
+            // Second feature: Pattern Matching
+            string ageFromConsole = "21";
+            int ageFromDatabase = 84;
+
+            object ageValue = ageFromConsole;
+
+            // pattern match here 'ageValue is int age' : if the variable contains an integer then store integer value in a new variable named age
+            // 'ageValue is string ageText && int.TryParse(ageText, out age)' : otherwise if it is string, convert the value temporarily stored in variable ageText
+            // into an integer.
+            if (ageValue is int age || (ageValue is string ageText && int.TryParse(ageText, out age)))
+            {
+                Console.WriteLine($"Your age is { age }.");
+            }
+            else
+            {
+                Console.WriteLine("We don't know your age.");
+            }
+
+
         }
     }
 }
